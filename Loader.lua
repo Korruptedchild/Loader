987eaf53c7ce2135ff79bedd23a0295c4b74b61b
Script Hub <151373064+Korruptedchild@users.noreply.github.com>
2025-09-04 17:53:02 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,7 +6,7 @@ if not your_key then error("No key provided!") end
 local entry
 for _,v in ipairs(Keys) do if v.Key==your_key then entry=v break end end
 if not entry then error("Invalid key!") end
-if entry.GameId~=game.GameId then error("Game not supported!") end
+if entry.GameId ~= game.PlaceId then error("Game not supported!") end
 
 local spinner={"|","/","-","\\"}
 local colors={BrickColor.new("Bright red"),BrickColor.new("Bright yellow"),BrickColor.new("Bright green"),BrickColor.new("Bright blue")}
